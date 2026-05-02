c5f9b57d303e68ad10634880d3d495f6dd7c53bb
QkiS-00 <vladpugac90@gmail.com>
2026-05-02 22:50:35 +0300

    add processStream with for-await loop, no full load into memory

--- lab6.js
+++ lab6.js
@@ -3,3 +3,15 @@ async function* generateLargeDataset(size) {
     yield { id: i, value: Math.random() * 1000 };
   }
 }
+
+async function processStream(asyncIterable) {
+  let count = 0;
+  let sum = 0;
+
+  for await (const item of asyncIterable) {
+    sum += item.value;
+    count += 1;
+  }
+
+  return { count, average: sum / count };
+}
